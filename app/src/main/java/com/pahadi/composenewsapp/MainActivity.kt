d9834eb814b7532fa946be2fb3ad3992285163c4
Aman Rawat <aman.rawat@purplle.com>
2025-02-18 14:02:53 +0530

    lets goo

--- app/src/main/java/com/pahadi/composenewsapp/MainActivity.kt
+++ app/src/main/java/com/pahadi/composenewsapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.pahadi.composenewsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,7 +15,6 @@ import com.pahadi.composenewsapp.ui.theme.ComposeNewsAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ComposeNewsAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
