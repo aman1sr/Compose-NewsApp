3f216f29acade272c9f620b52393b64776c8c1cf
Aman Rawat <aman.rawat@purplle.com>
2025-02-18 13:45:05 +0530

    entry point

--- app/src/main/java/com/pahadi/composenewsapp/MainActivity.kt
+++ app/src/main/java/com/pahadi/composenewsapp/MainActivity.kt
@@ -3,27 +3,29 @@ package com.pahadi.composenewsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+//import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.pahadi.composenewsapp.screens.NewsScreen
 import com.pahadi.composenewsapp.ui.theme.ComposeNewsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             ComposeNewsAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NewsScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
